ee6df413bcb835529a513e37246a02a08f0e596c
Tanvir-786770 <786770@pdsb.net>
2024-10-06 19:00:01 -0400

    Update U2 L5.py

--- U2 L5.py
+++ U2 L5.py
@@ -3,5 +3,5 @@ import math
 n = int(input("Please enter a non-zero whole number: "))
 print("j   tri       factorial")
 print("-----------------------")
-for i in range(1, upper + 1):
+for i in range(1, n + 1):
     print("%d   %d         %d" %(i, i**2, math.factorial(i)))
