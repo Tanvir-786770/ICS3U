d8218b7a0c020b72198550df5fe5dc0efdd759f4
Tanvir-786770 <786770@pdsb.net>
2024-10-15 10:26:42 -0400

    Update U3 L6 (unfinished).py

--- U3 L6 (unfinished).py
+++ U3 L6 (unfinished).py
@@ -2,15 +2,25 @@ import math as m
 
 def factorize(N):
   y = []
-  x = m.sqrt(N)
-  for j in range(1, m.floor(x)):
+  x = m.floor(m.sqrt(N))
+  for j in range(1, x + 1):
     if (N % j == 0):
       y.append(j)
+  for i in y:
+    if ((N % i == 0) and (i != 1)):
+      y.append(N/i)
   if (y == []):
     return "This number has no proper factors."
   else:
     return y
       
+
+def sum(a):
+  s = 0
+  for g in a:
+    s += g
+  return s
+
 
 # test values to test your program
 print(factorize(6))
@@ -18,3 +28,11 @@ print(factorize(24))
 print(factorize(0))
 print(factorize(1))
 print(factorize(7))
+
+x = 1
+while x == 1:
+  num = int(input("Enter a number: "))
+  h = factorize(num)
+  print(h)
+  f = sum(h)
+  print(f)
