0c8f4780c84a7ba01f4c2eac35d8c956daf881d7
Tanvir-786770 <786770@pdsb.net>
2024-11-15 10:53:09 -0500

    Create U5 L2 (unfinished).py

---
+++ U5 L2 (unfinished).py
@@ -0,0 +1,25 @@
+filename = "file.txt"
+fh = open(filename, "r")
+
+colorData = fh.readline()
+colorData = colorData.strip()
+
+rows, cols, numColors = colorData.split()
+
+rows = int(rows)
+cols = int(cols)
+numColors = int(numColors)
+
+# Using arrays
+colorDefs = [[0] * 2] * numColors # declare the array
+for i in range(numColors):
+   colorLine = fh.readline() # file handle must be open
+   colorLine.strip() 
+   sym, c, color = colorLine.split()
+   if sym == "~":
+       sym = " "
+   colorDefs[i][0] = sym
+   colorDefs[i][1] = color
+   print(colorDefs[i][0], ":", colorDefs[i][1])
+
+fh.close()
