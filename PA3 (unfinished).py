59725ea47c9bc0d3476f20a32d691e742e5342df
Tanvir-786770 <786770@pdsb.net>
2024-11-26 09:33:40 -0500

    Update PA3 (unfinished).py

--- PA3 (unfinished).py
+++ PA3 (unfinished).py
@@ -7,18 +7,30 @@ def plotit(x, y, d, color):
     t.dot(d, color)
     t.penup()
 
-filename = "rocky_bullwinkle_mod.xpm"
-fh = open(filename, "r")
+print("Welcome to the graphics plotter!")
+print("This program will plot an image using an inputted file!")
+
+x = 0
+while x == 0:
+    try:
+        x = 1
+        filename = input("\nTo begin, please enter the name of your image .txt/.xpm file: ")
+        fh = open(filename, "r")
+    except:
+        print("ERROR: File not found! Please try again!")
+        x = 0
 
 colorData = fh.readline()
 colorData = colorData.strip()
 
-rows, cols, numColors = colorData.split()
+cols, rows, numColors = colorData.split()
 
 rows = int(rows)
 cols = int(cols)
 numColors = int(numColors)
+print("\nFILE:", filename)
 print("%d by %d; %d colors." %(rows,cols,numColors))
+
 
 myColors = {
     
@@ -40,18 +52,17 @@ for x in range(rows):
     line = line.replace('\n', '')
     imagearray.append(line)
 
+print("\nThe image is now being plotted. Please wait...")
+print("HINT: You will see the image on the 'Python Turtle Graphics' window.")
 t.penup()
-t.goto(-(rows/2), cols/2)
-
 t.hideturtle()
 t.tracer(0, 0)
-print(myColors)
-print(imagearray)
 for x in range(cols):
     for y in range(rows):
         symbol = imagearray[y][x]
         color = myColors[symbol]
-        plotit((-(cols/2)+(x-1)), ((rows/2)-(y+1)), 3, color)
+        plotit((-(cols/2)+(x-1))*2, ((rows/2)-(y+1))*2, 3, color)
 t.update()        
 
+print("\nThe image has been plotted! Please check the 'Python Turtle Graphics' window!")
 fh.close()
