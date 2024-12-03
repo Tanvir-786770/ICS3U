dac65732bac08c5e82168db2aa8ef6c62bdc3c70
Tanvir-786770 <786770@pdsb.net>
2024-12-03 10:45:24 -0500

    Update PA3 (unfinished).py

--- PA3 (unfinished).py
+++ PA3 (unfinished).py
@@ -2,24 +2,11 @@
 Tanvir Rahim
 ICS3U - Programming Assignment 3
 Due 12/06/2024
+
 """
 
 
 import turtle as t
-
-def flipimage(upsidedown):
-    if upsidedown == 'Y' or upsidedown == 'y':
-        upsidedown == 'N'
-    else:
-        upsidedown == 'Y'
-    return upsidedown
-
-def changebgcolor(lightbg):
-    if lightbg == 'Y' or lightbg == 'y':
-        lightbg = 'N'
-    else:
-        lightbg = 'Y'
-    return lightbg
 
 def ynprompt(question):
     x = 1
@@ -39,14 +26,36 @@ def turtlesetup(bgcolor):
   
 def plotitUpright(cols, rows, x, y, d, color):
     t.penup()
-    t.goto((-(cols/2)+x) * 2, ((rows/2)-y) * 2)
+    xcord = (-(cols/2)+x) * 2
+    ycord = ((rows/2)-y) * 2
+    t.goto(xcord, ycord)
     t.pendown()
     t.dot(d, color)
     t.penup()
     
 def plotitUpsidedown(cols, rows, x, y, d, color):
     t.penup()
-    t.goto(((cols/2)-x) * 2, (-(rows/2)+y) * 2)
+    xcord = (-(cols/2)+x) * 2
+    ycord = ((rows/2)-y) * 2
+    t.goto(-(xcord), -(ycord))
+    t.pendown()
+    t.dot(d, color)
+    t.penup()
+
+def plotit90deg(cols, rows, x, y, d, color):
+    t.penup()
+    xcord = (-(cols/2)+x) * 2
+    ycord = ((rows/2)-y) * 2
+    t.goto(ycord, -(xcord))
+    t.pendown()
+    t.dot(d, color)
+    t.penup()
+    
+def plotit270deg(cols, rows, x, y, d, color):
+    t.penup()
+    xcord = (-(cols/2)+x) * 2
+    ycord = ((rows/2)-y) * 2
+    t.goto(-(ycord), xcord)
     t.pendown()
     t.dot(d, color)
     t.penup()
@@ -96,7 +105,18 @@ for x in range(rows):
     line = line.replace('\n', '')
     imagearray.append(line)
 
-upsidedown = ynprompt("\nWould you like your image to be upside down?")
+rotation = ynprompt("Would you like your image rotated?")
+
+if rotation == 'Y' or rotation == 'y':
+    q = 0
+    while q == 0:
+        rchoice = int(input("How would you like your image rotated (in degrees)? [90/180/270]: "))
+        if rchoice == 90 or rchoice == 180 or rchoice == 270:
+            q = 1
+        else:
+            print("Invalid input; try again.")
+else:
+    rchoice = 0
 
 print("The background is currently set as 'dark' (gray40).")
 lightbg = ynprompt("Would you like the 'light' background (gray70) instead?")
@@ -104,48 +124,41 @@ lightbg = ynprompt("Would you like the 'light' background (gray70) instead?")
 print("\nThe image is now being plotted. Please wait...")
 print("HINT: You will see the image on the 'Python Turtle Graphics' window.")
 
-i = 0
-while i == 0:
-    if lightbg == 'Y' or lightbg == 'y':
-        turtlesetup("gray70")
-    else:
-        turtlesetup("gray40")
+
+if lightbg == 'Y' or lightbg == 'y':
+    turtlesetup("gray70")
+else:
+    turtlesetup("gray40")
         
-    if upsidedown == 'Y' or upsidedown == 'y':
-        for x in range(cols):
-            for y in range(rows):
-                symbol = imagearray[y][x]
-                color = myColors[symbol]
-                plotitUpsidedown(cols, rows, x, y, 3, color)
-    else:
-        for x in range(cols):
-            for y in range(rows):
-                symbol = imagearray[y][x]
-                color = myColors[symbol]
-                plotitUpright(cols, rows, x, y, 3, color)
-    print("\nThe image has been plotted. Awaiting canvas update...")
-    t.update()
-    print("The image has been displayed! Please check the 'Python Turtle Graphics' window!")
-    
-    proceed = ynprompt("Would you like to make changes to the image, or exit the program?")
-        
-    if proceed == 'Y' or proceed == 'y':
-        a = 0
-        while a == 0:
-            change = input("Flip image, change background darkness, or both? [Flip/BG/Both]")
-            if change == "Flip" or change == "BG" or change == "Both":
-                a = 1
-            else:
-                print("Invalid input; try again.")
-        if change == 'Flip':
-            upsidedown = flipimage(upsidedown)
-        elif change == 'BG':
-            lightbg = changebgcolor(lightbg)
-        elif change == 'Both':
-            upsidedown = flipimage(upsidedown)
-            lightbg = changebgcolor(lightbg)
-        t.clear()
-    else:
-        i = 1
-        print("Thank you for using this program! :)")
+if rchoice == 90:
+    for x in range(cols):
+        for y in range(rows):
+            symbol = imagearray[y][x]
+            color = myColors[symbol]
+            plotit90deg(cols, rows, x, y, 3, color)
+
+elif rchoice == 180:
+    for x in range(cols):
+        for y in range(rows):
+            symbol = imagearray[y][x]
+            color = myColors[symbol]
+            plotitUpsidedown(cols, rows, x, y, 3, color)
+            
+elif rchoice == 270:
+    for x in range(cols):
+        for y in range(rows):
+            symbol = imagearray[y][x]
+            color = myColors[symbol]
+            plotit270deg(cols, rows, x, y, 3, color)
+else:
+    for x in range(cols):
+        for y in range(rows):
+            symbol = imagearray[y][x]
+            color = myColors[symbol]
+            plotitUpright(cols, rows, x, y, 3, color)
+
+print("\nThe image has been plotted. Awaiting canvas update...")
+t.update()
+print("The image has been displayed! Please check the 'Python Turtle Graphics' window!")
+print("\nThank you for using this program! :)")
 fh.close()
