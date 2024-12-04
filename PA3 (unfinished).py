7b1cd89458d267a271efe85937a739024ffa33a3
Tanvir-786770 <786770@pdsb.net>
2024-12-04 09:33:48 -0500

    Update PA3 (unfinished).py

--- PA3 (unfinished).py
+++ PA3 (unfinished).py
@@ -3,17 +3,57 @@ Tanvir Rahim
 ICS3U - Programming Assignment 3
 Due 12/06/2024
 
+Variable Dictionary:
+
+    t - Variable representing the turtle library.
+    yn - A value of 0 that does not change to 1 until the user inputs a valid yes/no answer.
+    ans - A variable that is assigned an inputted yes/no string value when the user is
+            prompted to answer a question.
+    bgcolor - A variable containing the name of the background color of the turtle graphics
+                plotter as a string.
+    xcord - The x-coordinate that the turtle plotter will move to in order to plot a point.
+    ycord - The y-coordinate that the turtle plotter will move to in order to plot a point.
+    d - The unit size of the point that will be plotted by turtle.
+    color - A certain color that the image has. This is referenced by the turtle plotter
+            in order for it to plot points with the right color.
+    f - A conditional value that does not change until the user inputs a valid file name.
+    filename - The name of the file that the program will read.
+    fh - The file handler; it is used for certain functions such as reading a line of the
+            file.
+    imageData - Contains information about the number of colors in the image, as well as the
+                number of columns and rows the image has in the data file.
+    cols - Value representing the number of columns the image has in the data file.
+    rows - Value representing the number of rows the image has in the data file.
+    numColors - Value representing the number of colors the image has.
+    myColors - A dictionary containing information about the symbols in the image data file
+                and the colors that they represent.
+    line - Represents a line of text of the data file.
+    sym - The symbol in the image data file that represents a certain color of the image.
+    c - The letter in the image data file that seperates the symbol from its corresponding
+        color. (It is simply used to get rid of itself).
+    imagearray - An array containing the actual data of the image (a whole bunch of symbols
+                    written out to actually form the image through text). These symbols
+                    represent a certain color of the image (the variable 'sym').
+    rotation - A variable containing the answer to a prompt asking the user if they would like
+                the image rotated.
+    rc - A value of 0 that does not change to 1 until the user inputs a valid response to
+            what kind of rotation they would like (90/180/270).
+    rchoice - A value containing the answer to a prompt asking the user what kind of rotation
+                they want.
+    
+    
+
 """
 
 
 import turtle as t
 
 def ynprompt(question):
-    x = 1
-    while x == 1:
+    yn = 1
+    while yn == 1:
         ans = input("%s [Y/N]: " %question)
         if ans == 'Y' or ans == 'y' or ans == 'N' or ans == 'n':
-            x = 0
+            yn = 0
         else:
             print("Invalid input; try again.")
     return ans
@@ -63,21 +103,21 @@ def plotit270deg(cols, rows, x, y, d, color):
 print("Welcome to the graphics plotter!")
 print("This program will plot an image using an inputted file!")
 
-x = 0
-while x == 0:
+f = 0
+while f == 0:
     try:
-        x = 1
         filename = input("\nTo begin, please enter the name of your '.xpm' file: ")
         fh = open(filename, "r")
+        f = 1
     except:
         print("ERROR: File not found! Please try again!")
         print("HINT: Try placing the '.xpm' file in the same folder as this python script.")
-        x = 0
+        f = 0
 
-colorData = fh.readline()
-colorData = colorData.strip()
+imageData = fh.readline()
+imageData = imageData.strip()
 
-cols, rows, numColors = colorData.split()
+cols, rows, numColors = imageData.split()
 
 rows = int(rows)
 cols = int(cols)
@@ -108,11 +148,11 @@ for x in range(rows):
 rotation = ynprompt("Would you like your image rotated?")
 
 if rotation == 'Y' or rotation == 'y':
-    q = 0
-    while q == 0:
+    rc = 0
+    while rc == 0:
         rchoice = int(input("How would you like your image rotated (in degrees)? [90/180/270]: "))
         if rchoice == 90 or rchoice == 180 or rchoice == 270:
-            q = 1
+            rc = 1
         else:
             print("Invalid input; try again.")
 else:
