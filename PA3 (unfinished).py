ed3ab8dff397daaed0f29722c14a5478f3648df4
Tanvir-786770 <786770@pdsb.net>
2024-12-04 19:12:21 -0500

    Update PA3 (unfinished).py

--- PA3 (unfinished).py
+++ PA3 (unfinished).py
@@ -34,44 +34,83 @@ Variable Dictionary:
     imagearray - An array containing the actual data of the image (a whole bunch of symbols
                     written out to actually form the image through text). These symbols
                     represent a certain color of the image (the variable 'sym').
-    rotation - A variable containing the answer to a prompt asking the user if they would like
-                the image rotated.
+    rotation - A variable containing a yes/no answer, as a string, to a prompt asking the user if
+                they would like the image rotated.
     rc - A value of 0 that does not change to 1 until the user inputs a valid response to
             what kind of rotation they would like (90/180/270).
-    rchoice - A value containing the answer to a prompt asking the user what kind of rotation
+    rchoice - A variable containing the answer to a prompt asking the user what kind of rotation
                 they want.
-    
-    
-
+    lightbg - A variable containing a yes/no answer, as a string, to a prompt asking the user if
+                they would like a lighter background (gray70) or keep it at default (gray40).
+    x - The variable that iterates through the number of columns of an image; it represents an
+        x coordinate of a particular symbol representing a point on the image in the data file.
+    y - The variable that iterates through the number of columns of an image; it represents a
+        y coordinate of a particular symbol representing a point on the image in the data file.
 """
 
-
 import turtle as t
+# Imports the Turtle Library and assigns it to 't'.
 
 def ynprompt(question):
-    yn = 1
-    while yn == 1:
+# Defines a function that prompts the user a yes/no question and expects a yes/no answer.
+# The paramter is the question that the user will be asked.
+    yn = 0
+    # As long as yn = 0, the user will be prompted with the question again until a valid
+    # ...yes/no response is received.
+    while yn == 0:   # The following loop will occur as long as yn = 0.
         ans = input("%s [Y/N]: " %question)
+        # Prompts the user with the question, and informs the user in a bracketed box that
+        # ...the user can either respond with 'Y' or 'N'. The response is saved in the
+        # ...'ans' variable.
         if ans == 'Y' or ans == 'y' or ans == 'N' or ans == 'n':
-            yn = 0
+        # If the answer provided to the prompted question is 'Y'/'y' (yes) or 'N'/'n' (no),
+        # ...the following will occur.
+            yn = 1
+            # The value of yn will be set to 1, meaning the while loop will end.
         else:
+        # If the answer provided to the prompted question is NOT a Y/N response, the
+        # ...following will occur.
             print("Invalid input; try again.")
+            # Lets the user know that the input is invalid, as it is not a Y/N answer.
     return ans
+    # Returns the value of 'ans', which is expected to either be 'Y'/'y' or 'N'/'n'.
 
 def turtlesetup(bgcolor):
-    t.penup()
+# Defines a subroutine that will set up the turtle plotter.
+# The parameter is the background color of the turtle plotter that the user chooses.
     t.hideturtle()
+    # Hides the turtle pointer/plotter from the screen to avoid obstruction of viewing
+    # ...the actual image.
+    t.penup()
+    # Lifts the turtle plotter from the canvas.
     t.bgcolor(bgcolor)
+    # Sets the background color to a certain gray background that the user chooses, which
+    # ...is represented by 'bgcolor'
     t.tracer(0, 0)
+    # Prevents the turtle plotter from displaying any actions (this speeds up the process
+    # ...of plotting the image on turtle).
+    # Later, the 'turtle.update()' function is executed, which updates the canvas after
+    # ...the entire image is plotted.
   
 def plotitUpright(cols, rows, x, y, d, color):
-    t.penup()
+# Defines a subroutine that plots a point when the image being plotted will be oriented
+# ...upright.
     xcord = (-(cols/2)+x) * 2
+    # The x-coordinate on the turtle canvas that the turtle plotter will plot a point on,
+    # ...based on the x-value of the image in its data value.
     ycord = ((rows/2)-y) * 2
+    # The y-coordinate on the turtle canvas that the turtle plotter will plot a point on,
+    # ...based on the y-value of the image in its data value.
     t.goto(xcord, ycord)
+    # The turtle plotter will move its posiition to the x/y coordinates given, which are
+    # ...the values of 'xcord' and 'ycord'.
     t.pendown()
+    # Sets the turtle plotter onto the canvas.
     t.dot(d, color)
+    # Plots a dot, with 'd' representing the size of the dot, and 'color' representing
+    # ...the color of the dot.
     t.penup()
+    # Lifts the turtle plotter up from the canvas.
     
 def plotitUpsidedown(cols, rows, x, y, d, color):
     t.penup()
