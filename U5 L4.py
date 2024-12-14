62bc6196edef27d862fd67d80ed46ef082269f61
Tanvir-786770 <786770@pdsb.net>
2024-12-14 18:20:00 -0500

    Add files via upload

---
+++ U5 L4.py
@@ -0,0 +1,39 @@
+def swap(B, p, q):
+    temp = B[p]
+    B[p] = B[q]
+    B[q] = temp
+
+def iSort(A):
+    for i in range(1, len(A)-1):
+        for j in range(0, i - 1):
+            if (A[i] < A[j]):
+                swap(A, i, j)
+
+def trim(word):
+    word = word.replace('\n', '')
+    return word
+
+def printIt(A):
+    for i in range(4):
+        for w in range(0 + (10*i), 10 + (10*i)):
+            print(A[w], end=' ')
+        print("\n")
+
+filename = '/Users/tanra/Downloads/words40.dat'
+wordarray = []
+
+fh = open(filename, "r")
+
+for i in range(40):
+    word = fh.readline()
+    word = trim(word)
+    wordarray.append(word)
+
+fh.close()
+
+iSort(wordarray)
+
+printIt(wordarray)
+
+print("\n%d words." %len(wordarray))
+
