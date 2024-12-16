bcac497845bb89abc40b34e5f2f27d9897334f95
Tanvir-786770 <786770@pdsb.net>
2024-12-16 08:58:18 -0500

    Create U5 L5.py

---
+++ U5 L5.py
@@ -0,0 +1,26 @@
+def getInt(prompt):
+    i = 0
+    while i == 0:
+      try:
+        ans = int(input(prompt))
+        if ans <= 0:
+          print("Invalid input. Try again.")
+        i = 1
+      except:
+        print("Invalid input. Try again.")
+        i = 0
+    return ans
+
+def fib(n):
+    if n == 0:
+      return 0
+    elif n == 1 or n == 2:
+      return 1
+    else:
+      return fib(n-1) + fib(n-2)
+
+# DO NOT ALTER ANY CODE BELOW
+tn = getInt("Please input a whole number: ")
+for i in range(1, tn + 1):
+    print(fib(i), end = " ")
+print()
